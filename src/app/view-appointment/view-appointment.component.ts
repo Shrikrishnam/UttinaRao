2303491f00f9b8129119b61f8ee58dc88ce703d5
Shrikrishnam <64524712+Shrikrishnam@users.noreply.github.com>
2020-08-21 18:43:36 +0530

    Add files via upload

---
+++ src/app/view-appointment/view-appointment.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services';
+import { allfriends } from '../_services/allfriends';
+import { Appoint } from '../place-fitness-trainer-appointment/place-fitness-trainer-appointment.component';
+import { throws } from 'assert';
+
+@Component({
+  selector: 'app-view-appointment',
+  templateUrl: './view-appointment.component.html',
+  styleUrls: ['./view-appointment.component.css']
+})
+export class ViewAppointmentComponent implements OnInit {
+
+  constructor(private us: UserService) { }
+
+  colunms = ["Sl NO","Name","Address","City","Package","Trainer Preference","Phone"];
+  //index = ["id","firstname","lastname","age","email","phonenumber","trainerpreference","physiotheropist","packages"];
+  list : Appoint[] = [];
+
+
+  ngOnInit() {
+    this.getAllAppointments();
+  }
+  getAllAppointments(){
+  this.us.getusers().subscribe
+    (
+      (response)=>
+      {
+        this.list = response;
+      },
+      (error)=>
+      {
+        console.log("Error :"+error);
+      }
+    )
+  }
+  deleteAppointment(id: number){
+    this.us.deleteUser(id).subscribe(
+      (data: allfriends)=>{
+        this.getAllAppointments();
+      }
+    );
+  }
+  editForm(appoint){
+   // document.getElementById("inr").innerHTML=appoint.inr;
+    document.getElementById("myForm").style.display = "block";
+    this.us.currentAppointment = Object.assign({}, appoint);
+
+  }
+  createAndUpdate(appoint: Appoint){
+    
+    this.us.updateUser(appoint).subscribe();
+
+    location.reload();
+  }
+  packageEvent(e){
+  
+    var x=e.target.value;
+    
+    this.us.currentAppointment.inr=x;
+    
+}
+
+}
